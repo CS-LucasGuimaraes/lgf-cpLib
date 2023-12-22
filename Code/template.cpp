23b721e15e26f5ede1d19d2e2523e680c90224ca
Lucas Guimarães <cs.lucas.guimaraes@gmail.com>
2023-12-22 08:32:26 -0300

    Base code template for competitive programming

---
+++ Code/template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+#define endl '\n'
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    int tc; cin >> tc; while (tc--) {
+        /* code */
+    }
+
+    return 0;
+}
