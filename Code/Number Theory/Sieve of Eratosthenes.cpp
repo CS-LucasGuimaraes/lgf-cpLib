88528158b0ec06d247f07f999eccae055bd775fa
Lucas Guimarães <cs.lucas.guimaraes@gmail.com>
2024-03-22 14:35:59 -0300

    Create Sieve of Eratosthenes.cpp

---
+++ Code/Number Theory/Sieve of Eratosthenes.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+vector<ll> prime_list;
+
+void SieveOfEratosthenes(ll n)
+{
+    vector<bool> prime(n + 1, true);
+ 
+    for (ll p = 2; p <= n; p++) {
+        if (prime[p] == true) {
+            prime_list.push_back(p);
+
+            for (ll i = p; i <= n; i += p)
+                prime[i] = false;
+        }
+    }
+}
